fa3cdc88401d162ce19eedca2eb23d167a8cab35
Parkman145 <parkerlearmonth@gmail.com>
2025-03-11 23:37:09 -0700

    Separate `AudioController` into its own file

---
+++ AudioController.py
@@ -0,0 +1,24 @@
+from pycaw.pycaw import AudioUtilities
+
+class AudioController:
+    def __init__(self, process_name):
+        self.process_name = process_name
+        self.volume = self.process_volume()
+
+    def process_volume(self):
+        sessions = AudioUtilities.GetAllSessions()
+        for session in sessions:
+            interface = session.SimpleAudioVolume
+            if session.Process and session.Process.name() == self.process_name:
+                # print("Volume:", interface.GetMasterVolume())  # debug
+                return interface.GetMasterVolume()
+
+    def set_volume(self, decibels):
+        sessions = AudioUtilities.GetAllSessions()
+        for session in sessions:
+            interface = session.SimpleAudioVolume
+            if session.Process and session.Process.name() == self.process_name:
+                # only set volume in the range 0.0 to 1.0
+                self.volume = min(1.0, max(0.0, decibels))
+                interface.SetMasterVolume(self.volume, None)
+                # print("Volume set to", self.volume)  # debug
