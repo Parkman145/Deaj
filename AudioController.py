c6c79e6f0b68fd63465a45a5982c526dfb9af260
Parkman145 <parkerlearmonth@gmail.com>
2025-03-27 17:04:44 -0700

    Implement `NoiseSuppressor`

--- AudioController.py
+++ AudioController.py
@@ -19,9 +19,10 @@ class NoiseSuppressor:
         self._threshold = threshold
 
 class AudioController:
-    def __init__(self, process_name):
+    def __init__(self, process_name, suppression_threshold = 0.01):
         self.process_name = process_name
         self.volume = self.process_volume()
+        self.suppressor = NoiseSuppressor(suppression_threshold)
 
     def process_volume(self):
         sessions = AudioUtilities.GetAllSessions()
@@ -32,11 +33,13 @@ class AudioController:
                 return interface.GetMasterVolume()
 
     def set_volume(self, decibels):
-        sessions = AudioUtilities.GetAllSessions()
-        for session in sessions:
-            interface = session.SimpleAudioVolume
-            if session.Process and session.Process.name() == self.process_name:
-                # only set volume in the range 0.0 to 1.0
-                self.volume = min(1.0, max(0.0, decibels))
-                interface.SetMasterVolume(self.volume, None)
-                # print("Volume set to", self.volume)  # debug
+        volume, update_volume = self.suppressor.get(decibels)
+        if update_volume:
+            sessions = AudioUtilities.GetAllSessions()
+            for session in sessions:
+                interface = session.SimpleAudioVolume
+                if session.Process and session.Process.name() == self.process_name:
+                    # only set volume in the range 0.0 to 1.0
+                    self.volume = min(1.0, max(0.0, decibels))
+                    interface.SetMasterVolume(self.volume, None)
+                    # print("Volume set to", self.volume)  # debug
